ad5f0bffe63a7eb6d6186fa70eb77c9bbc8c99d1
diakh19 <diya_khandelwal@optum.com>
2024-06-02 11:54:11 +0530

    Update Home.jsx

--- masterList-frontend/src/components/Home.jsx
+++ masterList-frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
     return (
         <div>
             <div style={{ backgroundImage: "url(src/images/train.jpg)", width: '100%', height: "100vh", backgroundRepeat: "round", backgroundsize: "cover", overflow: "scroll" }}>
-                <div className="card " style={{ width: "20rem", margin: "auto", marginTop: "15%" ,marginBottom:"80px"}}>
+                <div className="card mx-auto" style={{ width: "20rem", marginTop: "15rem" }}>
                     <img className="card-img-top" src="src/images/passenger.png" alt="Card image cap"></img>
                     <div className="card-body text-center">
                         <h5 className="card-title">My Profile</h5>
